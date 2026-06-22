1a9ca7e21a386072cd1f0eb8e3dfdd2811df5ce3
vikas12900 <121823542+vikas12900@users.noreply.github.com>
2026-06-23 01:33:55 +0530

    Delete utils directory

--- utils/embeddings.py
+++
@@ -1,6 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-def get_embeddings():
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-    )
--- utils/vector_store.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.vectorstores import FAISS
-
-def create_vector_store(docs, embeddings):
-    return FAISS.from_documents(
-        docs,
-        embeddings
-    )
